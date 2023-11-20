89228d764ba69c38843c19caaa91f759ffbd58c8
Aiden Bai <aiden.bai05@gmail.com>
2023-11-19 22:08:42 -0800

    init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import million from 'million/compiler';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
-  plugins: [million.vite(), react()],
+  plugins: [million.vite(), react(), viteSingleFile()],
 });
