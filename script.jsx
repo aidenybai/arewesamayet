dd74c6b1d13670acf69d070bb2db3a2ecb235f7a
Aiden Bai <aiden.bai05@gmail.com>
2023-11-19 22:06:53 -0800

    init

--- script.jsx
+++ script.jsx
@@ -3,7 +3,14 @@ import { createRoot } from 'react-dom/client';
 
 function App() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        paddingLeft: 'auto',
+        paddingRight: 'auto',
+      }}
+    >
       <h1>its so over 💀</h1>
       <p>
         current status: emmet shear is ceo{' '}
@@ -13,9 +20,17 @@ function App() {
       </p>
       <img
         src="/favicon.jpeg"
+        alt="sama"
+        loading="lazy"
         width={300}
+        height={400}
         style={{ marginLeft: 'auto', marginRight: 'auto' }}
       />
+      <br />
+      <small style={{ marginLeft: 'auto', marginRight: 'auto' }}>
+        how is this so fast? website powered by react.js +{' '}
+        <a href="https://million.dev">million.js</a>
+      </small>
     </div>
   );
 }
